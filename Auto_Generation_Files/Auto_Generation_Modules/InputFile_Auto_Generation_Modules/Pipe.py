25e5e5812bb96665147e3e4fcb7c9636a9a06043
junyoo96 <junyoo96@gmail.com>
2020-05-04 19:05:19 +0900

    Update Pipe.py

--- Auto_Generation_Files/Auto_Generation_Modules/InputFile_Auto_Generation_Modules/Pipe.py
+++ Auto_Generation_Files/Auto_Generation_Modules/InputFile_Auto_Generation_Modules/Pipe.py
@@ -7,6 +7,7 @@ class Pipe(Utility):
         self.pipe_material = pipe_material
         self.pipe_dielectric_smoothing_activation = pipe_dielectirc_smoothing_activation
         self.pipe_pass_point_y_range_offset = 0.02
+        self.pipe_pass_point_x_range_offset = 0.02
         self.pipe_thickness = 0.2
         self.water=water
         self.pipe_content_thickness = 0.1
@@ -20,62 +21,99 @@ class Pipe(Utility):
         domain_center_point_y = domain_y / 2
         offset_y_between_antenna_and_center=domain_center_point_y-antenna_y_offset
 
-        pipe_pass_point_x=domain_x/2
+        pipe_pass_point_x_min=domain_x/2-self.pipe_pass_point_x_range_offset
+        pipe_pass_point_x_max=domain_x/2+self.pipe_pass_point_x_range_offset
+        pipe_pass_point_x=utility.random_sampling(pipe_pass_point_x_min,pipe_pass_point_x_max)
+
         pipe_pass_point_y_min=antenna_y_offset+self.pipe_pass_point_y_range_offset
         pipe_pass_point_y_max=domain_center_point_y+(offset_y_between_antenna_and_center-self.pipe_pass_point_y_range_offset)
         pipe_pass_point_y=utility.random_sampling(pipe_pass_point_y_min,
                                                   pipe_pass_point_y_max)
 
-        #2. Pipe 시작 점 결정
-        pipe_start_point_x=utility.random_sampling(0,1)
-        pipe_start_point_y=0
-        #x가 0이면 y가 랜덤
-        if pipe_start_point_x==0:
-            pipe_start_point_y=utility.random_sampling(0,domain_y)
-        #x가 1이면 x가 랜덤이고, y=0
-        else:
-            pipe_start_point_x=utility.random_sampling(0,domain_x)
 
-        #3. Pipe 끝 점 결정
-        m=Symbol('m')
-        c=Symbol('c')
-        tmp_x=pipe_start_point_x
-        tmp_y=pipe_start_point_y
-        equation1 = m * tmp_x + c - tmp_y
-        tmp_x = pipe_pass_point_x
-        tmp_y = pipe_pass_point_y
-        equation2 = m * tmp_x + c - tmp_y
-
-        solution=solve((equation1,equation2),dict=True)
-
-        pipe_end_point_x = None
-        pipe_end_point_y = None
-        if pipe_start_point_x==0:
-            pipe_end_point_x = domain_x
-            pipe_end_point_y = solution[0][m] * pipe_end_point_x + solution[0][c]
-        else:
-            pipe_end_point_y = domain_y
-            pipe_end_point_x = (pipe_end_point_y - solution[0][c])/solution[0][m]
-
-        pipe_end_point_x=round(pipe_end_point_x,2)
-        pipe_end_point_y=round(pipe_end_point_y,2)
-
-        if pipe_end_point_x>domain_x:
-            pipe_end_point_x=domain_x
-        elif pipe_end_point_x<0:
-            pipe_end_point_x=0
-
-        if pipe_end_point_y>domain_y:
+        pipe_direction=utility.random_sampling(0,1)
+        pipe_start_point_x =0
+        pipe_start_point_y =0
+        pipe_end_point_x =0
+        pipe_end_point_y =0
+        #x축으로 길게
+        if pipe_direction==0:
+            pipe_start_point_x=pipe_pass_point_x
+            pipe_start_point_y=0
+            pipe_end_point_x=pipe_pass_point_x
             pipe_end_point_y=domain_y
-        elif pipe_end_point_y<0:
-            pipe_end_point_y=0
+        #y축으로 길게
+        else:
+            pipe_start_point_x =0
+            pipe_start_point_y =pipe_pass_point_y
+            pipe_end_point_x =domain_x
+            pipe_end_point_y =pipe_pass_point_y
+
+
+        pipe_start_point_z = utility.random_sampling(0.25,0.35)
+        pipe_end_point_z = pipe_start_point_z
+
+
+        #1. Pipe 지나갈 점 결정
+        # domain_center_point_y = domain_y / 2
+        # offset_y_between_antenna_and_center=domain_center_point_y-antenna_y_offset
+        #
+        # pipe_pass_point_x=domain_x/2
+        # pipe_pass_point_y_min=antenna_y_offset+self.pipe_pass_point_y_range_offset
+        # pipe_pass_point_y_max=domain_center_point_y+(offset_y_between_antenna_and_center-self.pipe_pass_point_y_range_offset)
+        # pipe_pass_point_y=utility.random_sampling(pipe_pass_point_y_min,
+        #                                           pipe_pass_point_y_max)
+
+        # 2. Pipe 시작 점 결정
+        # pipe_start_point_x = utility.random_sampling(0, 1)
+        # pipe_start_point_y = 0
+        # # x가 0이면 y가 랜덤
+        # if pipe_start_point_x == 0:
+        #     pipe_start_point_y = utility.random_sampling(0, domain_y)
+        # # x가 1이면 x가 랜덤이고, y=0
+        # else:
+        #     pipe_start_point_x = utility.random_sampling(0, domain_x)
+
+
+        # #3. Pipe 끝 점 결정( Pipe 대각선으로 하는 코드 일단 지우지마라)
+        # m=Symbol('m')
+        # c=Symbol('c')
+        # tmp_x=pipe_start_point_x
+        # tmp_y=pipe_start_point_y
+        # equation1 = m * tmp_x + c - tmp_y
+        # tmp_x = pipe_pass_point_x
+        # tmp_y = pipe_pass_point_y
+        # equation2 = m * tmp_x + c - tmp_y
+        #
+        # solution=solve((equation1,equation2),dict=True)
+        #
+        # pipe_end_point_x = None
+        # pipe_end_point_y = None
+        # if pipe_start_point_x==0:
+        #     pipe_end_point_x = domain_x
+        #     pipe_end_point_y = solution[0][m] * pipe_end_point_x + solution[0][c]
+        # else:
+        #     pipe_end_point_y = domain_y
+        #     pipe_end_point_x = (pipe_end_point_y - solution[0][c])/solution[0][m]
+        #
+        # pipe_end_point_x=round(pipe_end_point_x,2)
+        # pipe_end_point_y=round(pipe_end_point_y,2)
+        #
+        # if pipe_end_point_x>domain_x:
+        #     pipe_end_point_x=domain_x
+        # elif pipe_end_point_x<0:
+        #     pipe_end_point_x=0
+        #
+        # if pipe_end_point_y>domain_y:
+        #     pipe_end_point_y=domain_y
+        # elif pipe_end_point_y<0:
+        #     pipe_end_point_y=0
 
         # print(round(pipe_start_point_x,2),round(pipe_start_point_y,2))
         # print(round(pipe_pass_point_x, 2), round(pipe_pass_point_y, 2))
         # print(round(pipe_end_point_x,2),round(pipe_end_point_y,2))
 
-        pipe_start_point_z=0.3
-        pipe_end_point_z=pipe_start_point_z
+
 
         #3. Pipe외관 생성
         pipe_cylinder=Cylinder(pipe_start_point_x,
@@ -90,9 +128,6 @@ class Pipe(Utility):
                     )
 
         pipe_cylinder.write_textfile(textfile)
-
-        pipe_start_point_z = 0.3
-        pipe_end_point_z = pipe_start_point_z
 
         pipe_content_cylinder=None
         if self.water==True:

8988953072205ac577ac537d2c4f077b85fbdbb8
inkyunk1 <inkyunk1@uci.edu>
2020-05-12 08:15:57 +0000

    Add files

--- Auto_Generation_Files/Auto_Generation_Modules/automation_underground_object.sh
+++ Auto_Generation_Files/Auto_Generation_Modules/automation_underground_object.sh
@@ -24,31 +24,31 @@ trace_number=10
 for underground_object_index in $(seq 1 $numbers_to_generate);
 do
     #generate inputfile
-    python $INPUT_FILE_AUTOMATION_MODULES_MAIN_PY_PATH $underground_object_type_to_generate $underground_object_index
+    #python $INPUT_FILE_AUTOMATION_MODULES_MAIN_PY_PATH $underground_object_type_to_generate $underground_object_index
 
-    # #run gprmax with inputfile    
-    # for entry in $WORKTABLE_PATH/*.in;
-    # do         
-    #     input_file_name=$entry
-    #     python -m gprMax $input_file_name -n $trace_number -gpu
-    # done
+    #run gprmax with inputfile    
+    for entry in $WORKTABLE_PATH/*.in;
+    do         
+        input_file_name=$entry
+        python -m gprMax $input_file_name -n $trace_number -gpu
+    done
 
-    # #make merged_out file ( remove other out files )
-    # python -m tools.outputfiles_merge $WORKTABLE_PATH/${underground_object_type_to_generate}_$underground_object_index --remove-files
+    #make merged_out file ( remove other out files )
+    python -m tools.outputfiles_merge $WORKTABLE_PATH/${underground_object_type_to_generate}_$underground_object_index --remove-files
     
-    # #make image file (convert merged_out file to image file)    
-    # sudo /usr/local/MATLAB/R2020a/bin/matlab -nodisplay -nosplash -nodesktop -r "run('./MergedFile_To_Image_Auto_Converter_Modules/plot_Bscan_ed.m');exit;"    
+    #make image file (convert merged_out file to image file)    
+    sudo /usr/local/MATLAB/R2020a/bin/matlab -nodisplay -nosplash -nodesktop -r "run('./MergedFile_To_Image_Auto_Converter_Modules/plot_Bscan_ed.m');exit;"    
 
-    #remove vti files
-    
-    # find $WORKTABLE_PATH/ -type f -name "*.vti" -delete
-    # find $WORKTABLE_PATH/ -type f -name "*.out" -delete
-    # find $WORKTABLE_PATH/ -type f -name "*.in" -delete
-
+    #move worktable files
     find $WORKTABLE_PATH/ -type f -not -name "${underground_object_type_to_generate}_${underground_object_index}_1.vti" -name "*.vti" -delete
     mv  $WORKTABLE_PATH/*.vti $INPUT_FILES_PATH
     mv  $WORKTABLE_PATH/*.in $INPUT_FILES_PATH
     mv  $WORKTABLE_PATH/*.out $MERGED_OUT_FILES_PATH
+
+    #remove vti files    
+    # find $WORKTABLE_PATH/ -type f -name "*.vti" -delete
+    # find $WORKTABLE_PATH/ -type f -name "*.out" -delete
+    # find $WORKTABLE_PATH/ -type f -name "*.in" -delete
     
 done
 
